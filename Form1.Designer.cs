c8dd35a6010ee5e822dad12a89c3fee8628b7604
wyf518www <79488467@qq.com>
2020-11-19 14:37:51 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace 河北干部网络学院挂机
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox_UserName = new System.Windows.Forms.TextBox();
+            this.textBox_Password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox_ClassList = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb_XueXiBan = new System.Windows.Forms.RadioButton();
+            this.rb_XuanXiu = new System.Windows.Forms.RadioButton();
+            this.rb_BiXiu = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label_StudySchedule = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.timer_StudyScheduleCheck = new System.Windows.Forms.Timer(this.components);
+            this.timer_StydyHeartBeat = new System.Windows.Forms.Timer(this.components);
+            this.label_Test = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox_UserName
+            // 
+            this.textBox_UserName.Location = new System.Drawing.Point(37, 17);
+            this.textBox_UserName.Name = "textBox_UserName";
+            this.textBox_UserName.Size = new System.Drawing.Size(120, 21);
+            this.textBox_UserName.TabIndex = 0;
+            // 
+            // textBox_Password
+            // 
+            this.textBox_Password.Location = new System.Drawing.Point(196, 17);
+            this.textBox_Password.Name = "textBox_Password";
+            this.textBox_Password.Size = new System.Drawing.Size(49, 21);
+            this.textBox_Password.TabIndex = 1;
+            this.textBox_Password.Text = "888888";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "账号";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "密码";
+            // 
+            // listBox_ClassList
+            // 
+            this.listBox_ClassList.FormattingEnabled = true;
+            this.listBox_ClassList.HorizontalScrollbar = true;
+            this.listBox_ClassList.ItemHeight = 12;
+            this.listBox_ClassList.Location = new System.Drawing.Point(19, 194);
+            this.listBox_ClassList.Name = "listBox_ClassList";
+            this.listBox_ClassList.Size = new System.Drawing.Size(330, 244);
+            this.listBox_ClassList.TabIndex = 4;
+            this.listBox_ClassList.SelectedIndexChanged += new System.EventHandler(this.listBox_ClassList_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "课程列表";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(254, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 21);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "登录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb_XueXiBan);
+            this.groupBox1.Controls.Add(this.rb_XuanXiu);
+            this.groupBox1.Controls.Add(this.rb_BiXiu);
+            this.groupBox1.Location = new System.Drawing.Point(19, 64);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(330, 45);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "课程类型";
+            // 
+            // rb_XueXiBan
+            // 
+            this.rb_XueXiBan.AutoSize = true;
+            this.rb_XueXiBan.Enabled = false;
+            this.rb_XueXiBan.Location = new System.Drawing.Point(136, 18);
+            this.rb_XueXiBan.Name = "rb_XueXiBan";
+            this.rb_XueXiBan.Size = new System.Drawing.Size(59, 16);
+            this.rb_XueXiBan.TabIndex = 0;
+            this.rb_XueXiBan.TabStop = true;
+            this.rb_XueXiBan.Text = "学习班";
+            this.rb_XueXiBan.UseVisualStyleBackColor = true;
+            // 
+            // rb_XuanXiu
+            // 
+            this.rb_XuanXiu.AutoSize = true;
+            this.rb_XuanXiu.Checked = true;
+            this.rb_XuanXiu.Location = new System.Drawing.Point(71, 18);
+            this.rb_XuanXiu.Name = "rb_XuanXiu";
+            this.rb_XuanXiu.Size = new System.Drawing.Size(59, 16);
+            this.rb_XuanXiu.TabIndex = 0;
+            this.rb_XuanXiu.TabStop = true;
+            this.rb_XuanXiu.Text = "选修课";
+            this.rb_XuanXiu.UseVisualStyleBackColor = true;
+            // 
+            // rb_BiXiu
+            // 
+            this.rb_BiXiu.AutoSize = true;
+            this.rb_BiXiu.Location = new System.Drawing.Point(6, 18);
+            this.rb_BiXiu.Name = "rb_BiXiu";
+            this.rb_BiXiu.Size = new System.Drawing.Size(59, 16);
+            this.rb_BiXiu.TabIndex = 0;
+            this.rb_BiXiu.TabStop = true;
+            this.rb_BiXiu.Text = "必修课";
+            this.rb_BiXiu.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label_StudySchedule);
+            this.groupBox2.Location = new System.Drawing.Point(19, 117);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(330, 42);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "学习进度";
+            // 
+            // label_StudySchedule
+            // 
+            this.label_StudySchedule.AutoSize = true;
+            this.label_StudySchedule.Location = new System.Drawing.Point(6, 22);
+            this.label_StudySchedule.Name = "label_StudySchedule";
+            this.label_StudySchedule.Size = new System.Drawing.Size(65, 12);
+            this.label_StudySchedule.TabIndex = 0;
+            this.label_StudySchedule.Text = "学习进度：";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox_UserName);
+            this.groupBox3.Controls.Add(this.textBox_Password);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(19, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(330, 45);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "账号";
+            // 
+            // timer_StudyScheduleCheck
+            // 
+            this.timer_StudyScheduleCheck.Enabled = true;
+            this.timer_StudyScheduleCheck.Interval = 60000;
+            this.timer_StudyScheduleCheck.Tick += new System.EventHandler(this.timer_StudyScheduleCheck_Tick);
+            // 
+            // timer_StydyHeartBeat
+            // 
+            this.timer_StydyHeartBeat.Interval = 30000;
+            this.timer_StydyHeartBeat.Tick += new System.EventHandler(this.timer_StydyHeartBeat_Tick);
+            // 
+            // label_Test
+            // 
+            this.label_Test.AutoSize = true;
+            this.label_Test.Location = new System.Drawing.Point(182, 166);
+            this.label_Test.Name = "label_Test";
+            this.label_Test.Size = new System.Drawing.Size(41, 12);
+            this.label_Test.TabIndex = 10;
+            this.label_Test.Text = "label4";
+            this.label_Test.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 473);
+            this.Controls.Add(this.label_Test);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox_ClassList);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "安比网络科技";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_UserName;
+        private System.Windows.Forms.TextBox textBox_Password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox_ClassList;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rb_XueXiBan;
+        private System.Windows.Forms.RadioButton rb_XuanXiu;
+        private System.Windows.Forms.RadioButton rb_BiXiu;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label_StudySchedule;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Timer timer_StudyScheduleCheck;
+        private System.Windows.Forms.Timer timer_StydyHeartBeat;
+        private System.Windows.Forms.Label label_Test;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 河北干部网络学院挂机
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string classID = "";//课程ID
+        private string webuserAgent = "";//代理
+        private CookieContainer cookieContainer_0 = null;//cookie
+        private string uuid = "";
+        private string ucid = "";
+        private double lesson_location = 15;//"cmi.core.lesson_location":"1953.0"
+        private int session_time = 27;//"cmi.core.session_time":"00:00:57" 
+        private DateTime last_learn_time = DateTime.Now;//"last_learn_time":"2020-11-18 10:48:45"
+        private long unknownId = 1576049078020;
+        private bool isNewLesson = true;
+
+        private bool LogIn()
+        {
+            try
+            {
+                string requestUriString = "http://www.hebgb.gov.cn/portal/j_security_check";
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(requestUriString);
+                httpWebRequest.Method = "POST";
+                httpWebRequest.Accept = "*/*;";
+                httpWebRequest.UserAgent = this.webuserAgent;
+                httpWebRequest.ContentType = "application/x-www-form-urlencoded";
+                httpWebRequest.AllowAutoRedirect = true;
+                httpWebRequest.CookieContainer = this.cookieContainer_0;
+                httpWebRequest.KeepAlive = false;
+                httpWebRequest.ProtocolVersion = HttpVersion.Version10;
+                string s = string.Format("j_uri=%2Fportal%2Findex%21index.action&yzm=not&j_username={0}++&j_password={1}&imgcode=",
+                    this.textBox_UserName.Text,
+                    this.textBox_Password.Text);
+                byte[] bytes = Encoding.UTF8.GetBytes(s);
+                httpWebRequest.ContentLength = (long)bytes.Length;
+                httpWebRequest.GetRequestStream().Write(bytes, 0, bytes.Length);
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream());
+                string text = streamReader.ReadToEnd();
+                httpWebResponse.Close();
+                streamReader.Close();
+                httpWebRequest.Abort();
+                httpWebResponse.Close();
+                this.cookieContainer_0 = httpWebRequest.CookieContainer;
+                if (text.Contains("欢迎您"))
+                {
+                    this.button1.Text = "登录成功";
+                    this.textBox_UserName.Enabled = false;
+                    this.textBox_Password.Enabled = false;
+                    this.button1.Enabled = false;
+                    Thread.Sleep(1000);
+                    return true;
+                }
+                else
+                {
+                    if (text.Contains("验证码输入错误"))
+                    {
+                        MessageBox.Show("登录失败！四位的数字验证码输入错误！");
+                        return false;
+                    }
+                    if (text.Contains("用户名或密码输入错误"))
+                    {
+                        MessageBox.Show("登录失败！用户名或密码输入错误！请先上官网 www.hebgb.gov.cn 验证一下！也可能是网站升级造成登录失败。");
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Text = ex.Message.ToString();
+                if (ex.Message.Contains("500"))
+                {
+                    MessageBox.Show("帐号登录状态异常1，请重新登录！期间不要在外面的浏览器登录帐号，不然会挤掉本程序的登录，造成学习失效！");
+                }
+                else if (ex.Message.Contains("操时"))
+                {
+                    MessageBox.Show("登录操时！");
+                }
+                else
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            return false;
+        }
+
+        private bool GetClassList()
+        {
+            string selClassUrl = "";
+            if (rb_BiXiu.Checked)
+            {
+                selClassUrl = "http://www.hebgb.gov.cn/student/course!list.action?course.course_type=0&init=yes";//必修
+            }
+            else if (rb_XuanXiu.Checked)
+            {
+                selClassUrl = "http://www.hebgb.gov.cn/student/course!list.action?course.course_type=1&init=yes";//选修
+            }
+            else
+            {
+                string classId = "0";//需获取
+                selClassUrl = "http://www.hebgb.gov.cn/student/clazz!main.action?init&clazz.id=" + classId;
+            }
+            try
+            {
+                string ClassPageTxt = GetUrl(selClassUrl);
+                if (!ClassPageTxt.Contains("欢迎您"))
+                {
+                    MessageBox.Show("帐号登录状态异常2，请重新登录！期间不要在外面的浏览器登录帐号，不然会挤掉本程序的登录，造成学习失效！");
+                    return false;
+                }
+
+                string str_Tmp = "<img class=\"img\" title=";
+                string[] array = ClassPageTxt.Split(new string[]
+                {
+                           str_Tmp
+                }, StringSplitOptions.None);
+                this.listBox_ClassList.Items.Clear();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i].Contains("images/index_images/ico014.png"))
+                    {
+                        int length = array[i].IndexOf("images/index_images/ico014.png");
+                        this.listBox_ClassList.Items.Add(array[i].Substring(0, length));
+                    }
+                }
+                if (this.listBox_ClassList.Items.Count != 0)
+                {
+                    this.listBox_ClassList.SelectedIndex = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString() + "  帐号登录状态异常2，请重新登录！期间不要在外面的浏览器登录帐号，不然会挤掉本程序的登录，造成学习失效！");
+                return false;
+            }
+            return true;
+        }
+
+        private string GetUrl(string url)
+        {
+            this.webuserAgent = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)";
+            if (cookieContainer_0 == null) { cookieContainer_0 = new CookieContainer(); }
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.Accept = "*/*";
+            httpWebRequest.Method = "GET";
+            httpWebRequest.UserAgent = this.webuserAgent;
+            httpWebRequest.ContentType = "application/x-www-form-urlencoded";
+            httpWebRequest.AllowAutoRedirect = true;
+            httpWebRequest.CookieContainer = this.cookieContainer_0;
+            httpWebRequest.KeepAlive = true;
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream());
+            string htmlText = streamReader.ReadToEnd();
+            httpWebResponse.Close();
+            streamReader.Close();
+            httpWebRequest.Abort();
+            httpWebResponse.Close();
+            this.cookieContainer_0 = httpWebRequest.CookieContainer;
+            return htmlText;
+        }
+        //如果有多个匹配值 结果未知
+        private string GetMidStr(string htmlText, string lhs, string rhs, bool hasLhs = false, bool hasRhs = false)
+        {
+            string str_Tmp = lhs;
+            string[] array = htmlText.Split(new string[] { str_Tmp }, StringSplitOptions.None);
+            string midStr = array.LastOrDefault().Split(new string[] { rhs }, StringSplitOptions.None).FirstOrDefault();
+            if (hasLhs) { midStr = lhs + midStr; }
+            if (hasRhs) { midStr += rhs; }
+            return midStr;
+        }
+
+        private void StartStudy()
+        {
+            int indexS = this.listBox_ClassList.Text.IndexOf(">");
+            int indexE = this.listBox_ClassList.Text.IndexOf("src=");
+            int strLength = indexE - indexS;
+            string strTmp = this.listBox_ClassList.Text.Substring(1, indexE - 3);
+            if (!rb_XueXiBan.Checked)
+            {
+                indexS = this.listBox_ClassList.Text.IndexOf("%;\"></span></span>");
+                indexE = this.listBox_ClassList.Text.IndexOf("%</span>");
+                int lengthTmp = "%;\"></span></span>".Length;
+                strLength = indexE - indexS - lengthTmp;
+                indexS = this.listBox_ClassList.Text.IndexOf("videoList(");
+                indexE = this.listBox_ClassList.Text.IndexOf(");");
+                lengthTmp = "videoList(".Length;
+                strLength = indexE - indexS - lengthTmp;
+                this.classID = this.listBox_ClassList.Text.Substring(indexS + lengthTmp, strLength);
+            }
+
+            string htmlText = GetUrl("http://www.hebgb.gov.cn/portal/study!play.action?id=" + this.classID);
+            if (htmlText.Contains("http://cdn.hebgb.gov.cn"))
+            {
+                string studyPercent = GetMidStr(htmlText, "<span style=\"margin-left: 5px;\">", "</span>");
+                studyPercent = studyPercent.Replace("\r", "");
+                studyPercent = studyPercent.Replace("\n", "");
+                studyPercent = studyPercent.Replace("%", "");
+                studyPercent = studyPercent.Replace(" ", "");
+                double.TryParse(studyPercent, out double percent);
+                if (percent > 99.9)
+                {
+                    timer_StydyHeartBeat.Stop();
+                    timer_StydyHeartBeat.Enabled = false;
+                }
+                this.label_StudySchedule.Text = "当前课程学习进度:" + percent + "%";
+
+                string realPlayUrl = GetMidStr(htmlText, "http://cdn.hebgb.gov.cn", "\"", true);
+                string realPlayUrlUnescape = Uri.UnescapeDataString(realPlayUrl);
+                string ucid_Tmp = GetMidStr(realPlayUrlUnescape, "\"ucid\":\"", "\"");
+                string uuid_Tmp = GetMidStr(realPlayUrlUnescape, "\"uuid\":\"", "\"");
+                if (uuid_Tmp != uuid && ucid_Tmp != ucid)
+                {
+                    isNewLesson = true;
+                    uuid = uuid_Tmp;
+                    ucid = ucid_Tmp;
+
+                    lesson_location = 15;//"cmi.core.lesson_location":"1953.0"
+                    session_time = 27;//"cmi.core.session_time":"00:00:57" 
+                    timer_StydyHeartBeat.Enabled = true;
+                    timer_StydyHeartBeat.Start();
+                }
+            }
+            else
+            {
+                MessageBox.Show("起床查BUG咯！");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.textBox_UserName.Text == "" || this.textBox_Password.Text == "")
+            {
+                MessageBox.Show("账号密码不能为空！");
+            }
+            else
+            {
+                this.listBox_ClassList.Items.Clear();
+
+                if (LogIn())
+                {
+                    if (GetClassList())
+                    {
+                        StartStudy();
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetUrl("http://www.hebgb.gov.cn/portal/index!index.action");
+        }
+        
+        private void listBox_ClassList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            StartStudy();
+        }
+
+        private void timer_StudyScheduleCheck_Tick(object sender, EventArgs e)
+        {
+            if (GetClassList())
+                StartStudy();
+        }
+
+        private void timer_StydyHeartBeat_Tick(object sender, EventArgs e)
+        {
+            if (isNewLesson)
+            {
+                lesson_location = 15.0;
+                last_learn_time = DateTime.Now;
+            }
+            isNewLesson = false;
+            DateTime timeNow = DateTime.Now;
+            long ticks = timeNow.Ticks - last_learn_time.Ticks;            
+            session_time = (session_time + 30) % 60;
+            lesson_location += 30;
+            string tickUrl = "http://www.hebgb.gov.cn/portal/study!seek.action?callback=showData&uuid="
+                + uuid + "&id=" + ucid
+                + "&serializeSco={\"0\":{\"cmi.core.exit\":\"logout\",\"cmi.core.lesson_location\":\""
+                + lesson_location.ToString("0.0")
+                + "\",\"cmi.core.session_time\":\"00:00:"
+                + session_time + "\",\"cmi.core.entry\":\"resume\",\"last_learn_time\":\""
+                + last_learn_time.ToString("yyyy-MM-dd HH:mm:ss") + "\"}}&duration=30000&a=提交&_="
+                + unknownId.ToString() + "\"";
+
+            label_Test.Text = GetUrl(tickUrl); 
+
+            ///*需要发送的包
+            //http://www.hebgb.gov.cn/portal/study!seek.action?callback=showData&
+            //uuid=331bc2bf-1e35-41cc-9748-897637156c18&
+            //id=62289911&
+            //serializeSco={"0":{"cmi.core.exit":"logout","cmi.core.lesson_location":"1953.0","cmi.core.session_time":"00:00:57","cmi.core.entry":"resume","last_learn_time":"2020-11-18 10:48:45"}}&
+            //duration=30000&
+            //a=提交&
+            //_=1605665805851*/
+        }
+    }
+}
